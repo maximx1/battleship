13a8891f45537230f9445296055a2d5427bc0f60
Brian Vanderbusch <brian@hackerhappyhour.com>
2015-09-18 22:10:39 -0500

    fix test spec missing it function

--- test/hello.spec.js
+++ test/hello.spec.js
@@ -3,6 +3,8 @@ var hello = require("../src/hello.js");
 
 describe("Hello World Test", function(){
   describe("#hello()", function(){
-	assert.equal("Hello World", hello.say(),"Basic test is working");  
+	it('should output Hello World',function(){
+	  assert.equal("Hello World", hello.say(),"Basic test is working");  	
+	});
   });
 });
