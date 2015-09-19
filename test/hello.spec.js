cd095424f285ff0ee6fe1c35456c9c878d9562a9
Justin Walrath <walrathjaw@gmail.com>
2015-09-19 15:53:42 -0500

    Using chai.

--- test/hello.spec.js
+++ test/hello.spec.js
@@ -1,10 +1,11 @@
 var assert = require("assert");
+var expect = require("chai").expect;
 var hello = require("../src/hello.js");
 
-describe("Hello World Test", function(){
-  describe("#hello()", function(){
-	it('should output Hello World',function(){
-	  assert.equal("Hello World", hello.say(),"Basic test is working");  	
-	});
+describe("Hello World Test", function() {
+  describe("#hello()", function() {
+  	it('should output Hello World',function(){
+      expect(hello.say()).to.equal("Hello World")
+  	});
   });
 });
